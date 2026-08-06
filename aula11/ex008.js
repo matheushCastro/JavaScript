77f93f485fe4edc4fe5a0daf460904dc770a3dbe
Matheus Henrique <mhenrique182005@gmail.com>
2026-08-06 17:23:42 -0300

    sistema de multas (usando condicoes

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel=100
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if (vel < 70){
+    console.log(`Velocidade ok para oque se pede na via `)
+
+}else{
+
+     console.log("Multado por excesso de velocidade permitido")
+    
+}
